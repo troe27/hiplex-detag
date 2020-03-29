69c62303fd4286141b56248b7edc860d563fbff2
Tilman <tilman.roenneburg@googlemail.com>
2020-03-29 23:27:44 +0200

    modified the bash files to run the script

---
+++ test_data/run_unpaired.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python ../detag.py --tags ./tags_no_CAA.txt --heel ./heel.txt --unpairedfastq first_100k_R1.fastq --pairedparams 7:5:10 --outputprefix ./trial73  --h5score 0.9 --h3score 0.9 --filtering 1:50:150:20:15 --gsp ./tabulated_relevant_primers.txt
